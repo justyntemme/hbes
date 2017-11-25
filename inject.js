05d9b61f1074973d0163862dae01e679ac5a2141
Justyn Temme <Justyntemme@gmail.com>
2017-11-25 15:02:00 -0600

    works with gucci.com. Now to add styling

--- inject.js
+++ inject.js
@@ -24,26 +24,25 @@
 
   }, function(items) {
 	  console.log(items);
+	  window.location.replace("https://www.gucci.com/us/en/checkout/single#step-payment");
 
-	document.getElementById('place-order-terms').checked = true;
+		document.getElementById('place-order-terms').checked = true;
+		document.getElementsByName('shipping_name')[0].value = items.shipping_name;
+		document.getElementsByName('shipping_last_name')[0].value = items.shipping_last_name;
+		document.getElementsByName('shipping_address_line_1')[0].value = items.shipping_address_line_1;
+		document.getElementsByName('shipping_address_line_2')[0].value = items.shipping_address_line_2;
+		document.getElementsByName('shipping_city')[0].value = items.shipping_city;
+		document.getElementsByName('shipping_state')[0].selectedIndex = items.shipping_state;
+		document.getElementsByName('shipping_zip_code')[0].value = items.shipping_zip_code;
+		document.getElementsByName('shipping_phone')[0].value = items.shipping_phone;
+		document.getElementsByName('payment-card-type')[0].selectedIndex = items.payment_credit_card_type;
+		
+		document.getElementsByName('card_cvn')[0].value = items.card_cvn;
+		//document.getElementsByName('payment-card-nameOnCard')[0].value = items.payment_card_nameOnCard
+		document.getElementById('card_expirationMonth').selectedIndex = items.card_expirationMonth;
+		document.getElementById('card_expirationYear').selectedIndex = items.card_expirationYear;
 
-    document.getElementsByName('shipping_name')[0].value = items.shipping_name;
-	document.getElementsByName('shipping_last_name')[0].value = items.shipping_last_name;
-    document.getElementsByName('shipping_address_line_1')[0].value = items.shipping_address_line_1;
-	document.getElementsByName('shipping_address_line_2')[0].value = items.shipping_address_line_2;
-	document.getElementsByName('shipping_city')[0].value = items.shipping_city;
-	document.getElementsByName('shipping_state')[0].selectedIndex = items.shipping_state;
-	document.getElementsByName('shipping_zip_code')[0].value = items.shipping_zip_code;
-	document.getElementsByName('shipping_phone')[0].value = items.shipping_phone;
-	document.getElementsByName('payment-card-type')[0].selectedIndex = items.payment_credit_card_type;
-	document.getElementsByName('card_number')[0].value = items.card_number;
-	document.getElementsByName('card_cvn')[0].value = items.card_cvn;
-	document.getElementsByName('payment-card-nameOnCard')[0].value = items.payment_card_nameOnCard
-	document.getElementById('card_expirationMonth').selectedIndex = items.card_expirationMonth;
-	document.getElementById('card_expirationYear').selectedIndex = items.card_expirationYear;
-
-
-
+		document.getElementsByName('card_number')[0].value = items.card_number;
 
    
   });
