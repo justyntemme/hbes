57a059f5b5ca2199aee7230049f7f8a18a955a9e
Justyn Temme <Justyntemme@gmail.com>
2017-11-25 12:41:38 -0600

    .

---
+++ inject.js
@@ -0,0 +1,35 @@
+// this is the code which will be injected into a given page... shipping_last_name
+
+(function() {
+
+	// just place a div at top right
+
+
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+      shipping_name: "name",
+	  shipping_last_name: "last name",
+	  shipping_address_line_1: "street address",
+	  shipping_address_line_2: "street address Line 2",
+	  shipping_city: "shipping_city",
+	  shipping_state: "state",
+	  shipping_zip_code: "zip",
+	  shipping_phone: "phone #"
+
+  }, function(items) {
+	  console.log(items);
+
+    document.getElementsByName('shipping_name')[0].value = items.shipping_name;
+	document.getElementsByName('shipping_last_name')[0].value = items.shipping_last_name;
+    document.getElementsByName('shipping_address_line_1')[0].value = items.shipping_address_line_1;
+	document.getElementsByName('shipping_address_line_2')[0].value = items.shipping_address_line_2;
+	document.getElementsByName('shipping_city')[0].value = items.shipping_city;
+	document.getElementsByName('shipping_state')[0].value = items.shipping_state;
+	document.getElementsByName('shipping_zip_code')[0].value = items.shipping_zip_code;
+	document.getElementsByName('shipping_phone')[0].value = items.shipping_phone;
+   
+  });
+
+
+
+})();
---
+++ options.js
@@ -0,0 +1,62 @@
+// shipping_address_line_2
+
+document.getElementsByName("save_options")[0].addEventListener("click", save_options);
+document.getElementsByName("restore_options")[0].addEventListener("click", restore_options);
+
+function save_options() {
+    var shipping_name = document.getElementById("shipping_name").value;
+    var shipping_last_name = document.getElementById("shipping_last_name").value;
+    var shipping_address_line_1 = document.getElementById("shipping_address_line_1").value;
+    var shipping_address_line_2 = document.getElementById("shipping_address_line_2").value;
+    var shipping_city = document.getElementById("shipping_city").value;
+    var shipping_state = document.getElementById("shipping_state").value;
+    var shipping_zip_code = document.getElementById("shipping_zip_code").value;
+    var shipping_phone = document.getElementById("shipping_phone").value;
+
+    chrome.storage.sync.set({
+        shipping_name: shipping_name,
+        shipping_last_name: shipping_last_name,
+        shipping_address_line_1: shipping_address_line_1,
+        shipping_address_line_2: shipping_address_line_2,
+        shipping_city: shipping_city,
+        shipping_state: shipping_state,
+        shipping_zip_code: shipping_zip_code,
+        shipping_phone: shipping_phone,
+    }, function() {
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({
+      shipping_name: "name",
+      shipping_last_name: "last name",
+      shipping_address_line_1: "Address Line 1",
+      shipping_address_line_2: "Address Line 2",
+      shipping_city: "City",
+      shipping_state: "State",
+      shipping_zip_code: "zip",
+      shipping_phone: "phone #"
+  }, function(items) {
+
+    document.getElementById('shipping_name').value = items.shipping_name;
+    document.getElementById('shipping_last_name').value = items.shipping_last_name;
+    document.getElementById('shipping_address_line_1').value = items.shipping_address_line_1;
+    document.getElementById('shipping_address_line_2').value = items.shipping_address_line_2;
+    document.getElementById('shipping_city').value = items.shipping_city;
+    document.getElementById('shipping_state').value = items.shipping_state;
+    document.getElementById('shipping_zip_code').value = items.shipping_zip_code;
+    document.getElementById('shipping_phone').value = items.shipping_phone;
+   
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save_options').addEventListener('click',
+    save_options);
